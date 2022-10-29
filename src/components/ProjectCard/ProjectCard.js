f9507224fa36c82390ec327c35ed55b3754c86c1
LuccaTambor <lucca.tambor@outlook.com>
2022-10-29 11:41:58 -0300

    feat: Adjustments to ProjectCard

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Moment from 'react-moment';
+import moment from 'moment';
 import {Link} from "react-router-dom";
 
 import './ProjectCard.css';
@@ -7,18 +8,38 @@ import './ProjectCard.css';
 function ProjectCard(props) {
   const projectDetailsUrl = "/detalhes-projeto/" + props.projectData.id;
 
+  const status = (date, startDate) => {
+    let expectedDate = moment(date);
+    let start = moment(startDate)
+    let today = moment();
+
+    console.log(expectedDate)
+
+    if(start > today)
+    return (<span>Não Iniciado</span>);
+    if(expectedDate > today)
+      return (<span>Em andamento</span>);
+    if(expectedDate < today)
+      return (<span>Atrasado</span>);
+  }
+
   return (
-    <div className="project-card">
+    <Link to={projectDetailsUrl} className="project-card">
       <div className="project-card-preview">
         <h6>Projeto</h6>
         <h2>{props.projectData.description}</h2>
       </div>
       <div className="project-card-info">
-        <h6>Prazo final: <Moment format="DD/MM/YYYY">{props.projectData.expectedFinishDate}</Moment></h6>
-        <h2>Alguma informação aqui</h2>
-        <Link className="btn" to={projectDetailsUrl} >Detalhes</Link>
+        <div className="dates">
+          <h6>Iniciado em: <Moment format="DD/MM/YYYY">{props.projectData.startedOn}</Moment></h6>
+          <h3>Prazo final: <Moment format="DD/MM/YYYY">{props.projectData.expectedFinishDate}</Moment></h3>
+        </div>
+        <div className="main-info">
+          <h3>Times: {props.projectData.teams.length}</h3>
+          <h3>Status: {status(props.projectData.expectedFinishDate, props.projectData.startedOn)}</h3>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
