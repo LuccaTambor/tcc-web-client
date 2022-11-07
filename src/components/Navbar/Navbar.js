5bfb0c69424520c350b96c33cb67594e832535e1
LuccaTambor <lucca.tambor@outlook.com>
2022-11-06 22:12:56 -0300

    feat: confirm exit modal

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,27 @@
 import React from "react";
 import SideNav, {Toggle, NavItem, NavIcon, NavText} from "@trendmicro/react-sidenav";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import Modal from 'react-modal';
 
 import './Navbar.css';
 import "@trendmicro/react-sidenav/dist/react-sidenav.css";
 
 function Navbar(props) {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const location = useLocation();
+  const pathNow = location.pathname.substring(1);
+  const [logoutModalOpen, setLogoutModalOpen] = React.useState(false);
+
+  const openLogouModal = () => {
+    setLogoutModalOpen(true);
+  }
+
+  const closeLogouModal = () => {
+    setLogoutModalOpen(false);
+  }
 
   return (
+    <div className="">
       <SideNav 
         onSelect={selected => {
           navigate('/'+selected)
@@ -45,7 +58,7 @@ function Navbar(props) {
             <NavText>OCORRÊNCIAS E DADOS</NavText>
           </NavItem>}
           <hr />
-          <NavItem eventKey="" onClick={props.toLogout}>
+          <NavItem eventKey={pathNow} onClick={openLogouModal}>
             <NavIcon>
                 <i className="fas fa-sign-out-alt" style={{ fontSize: '1.75em' }} />
             </NavIcon>
@@ -53,6 +66,22 @@ function Navbar(props) {
           </NavItem>
         </SideNav.Nav>
       </SideNav>
+      <Modal
+        isOpen={logoutModalOpen}
+        onRequestClose={closeLogouModal}
+        contentLabel="Example Modal"
+        className="modal logout-modal"
+        overlayClassName="overlay"
+      >
+        <span onClick={closeLogouModal} className="close-btn"><i className="fas fa-times"></i></span>
+        <h3>Deseja realmente sair?</h3>
+        <div className="btns-sec">
+          <button className="btn-danger" onClick={props.toLogout}>Sim <i className="fas fa-times"></i></button>
+          <button className="btn-create" onClick={closeLogouModal}>Não</button>
+        </div>
+      </Modal>
+    </div>
+      
   )
 }
 export default Navbar;
