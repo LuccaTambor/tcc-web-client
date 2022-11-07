e5dabb450d0b2b3dd5f9ee6656e4ca8c64d909d2
LuccaTambor <lucca.tambor@outlook.com>
2022-11-07 11:58:34 -0300

    fix: image test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import LogoMain from "../../assets/images/Logo.png";
+//import LogoMain from "../../assets/images/Logo.png";
 
 import "./Home.css";
 
@@ -8,7 +8,7 @@ function Home(props) {
   return(
     <div className="home">
       <div className="home-info">
-        <img alt="logo principal" src={LogoMain} className="home-logo"/> 
+        {/* <img alt="logo principal" src={ require('../../assets/images/Logo.png').default} className="home-logo"/>  */}
         <p className="home-text">Bem vindo ao GPMAA {props.userData.name}</p>
         <p className="home-text">{props.isManager() ? '[Gerente]' : '[Desenvolvedor]'}</p>
       </div> 
