272f15fbb14a736b0c5b5f9d29dc902af42373ba
LuccaTambor <lucca.tambor@outlook.com>
2022-11-08 20:25:15 -0300

    feat: treat delete project with teams case

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -2,11 +2,22 @@ import React from "react";
 import Moment from 'react-moment';
 import moment from 'moment';
 import {Link} from "react-router-dom";
+import Modal from 'react-modal';
 
 import './ProjectCard.css';
 
 function ProjectCard(props) {
   const projectDetailsUrl = "/detalhes-projeto/" + props.projectData.id;
+
+  const [warningModalOpen, setWarningModalOpen] =React.useState(false);
+
+  const openWarning = () => {
+    setWarningModalOpen(true);
+  }
+
+  const closeWarning = () => {
+    setWarningModalOpen(false);
+  }
 
   const status = (date, startDate) => {
     let expectedDate = moment(date);
@@ -19,6 +30,13 @@ function ProjectCard(props) {
       return (<span>Em andamento</span>);
     if(expectedDate < today)
       return (<span>Atrasado</span>);
+  }
+
+  const tryToDelete = () => {
+    if(props.projectData.teams.length > 0)
+      openWarning();
+    else
+      props.toDelete(props.projectData.id);
   }
 
   return (
@@ -37,11 +55,24 @@ function ProjectCard(props) {
           <h3>Status: {status(props.projectData.expectedFinishDate, props.projectData.startedOn)}</h3>
           <div className="buttons-section">   
             <Link to={projectDetailsUrl} className="btn-primary">Detalhes</Link>
-            {props.isManager() && <button className="btn-danger" onClick={() => props.toDelete(props.projectData.id)}><i className="fas fa-trash"></i></button>}  
+            {props.isManager() && <button className="btn-danger" onClick={tryToDelete}><i className="fas fa-trash"></i></button>}  
           </div>
-          
+        
         </div>
       </div>
+      <Modal
+        isOpen={warningModalOpen}
+        onRequestClose={closeWarning}
+        contentLabel="Example Modal"
+        className="modal warning-modal"
+        overlayClassName="overlay"
+      >
+        <div className="warning">
+          <span className="warning-icon"><i className="fas fa-times"></i></span>
+          <h3 className="warning-text">Não é possível apagar um projeto com times em atividade</h3>
+          <button className="btn-danger" onClick={closeWarning}>OK</button>
+        </div>
+      </Modal>
     </div>
   )
 }
